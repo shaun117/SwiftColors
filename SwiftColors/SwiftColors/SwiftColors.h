8e0a19de217f6bcac016a61f5e34b5a19f161c63
shaun117 <shaun.mataire@live.com>
2015-07-09 22:14:09 -0500

    SwiftColors Init

---
+++ SwiftColors/SwiftColors/SwiftColors.h
@@ -0,0 +1,19 @@
+//
+//  SwiftColors.h
+//  SwiftColors
+//
+//  Created by Shaun Mataire on 7/9/15.
+//  Copyright (c) 2015 Boxardev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftColors.
+FOUNDATION_EXPORT double SwiftColorsVersionNumber;
+
+//! Project version string for SwiftColors.
+FOUNDATION_EXPORT const unsigned char SwiftColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftColors/PublicHeader.h>
+
+
